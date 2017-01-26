3ff703e39ad1f862878915da0c293ca780675933
Caglayan, Erdem <erdemcaglayan_1@hotmail.com>
2017-01-26 21:59:57 +0200

    JTAExecuter project created.

---
+++ JTAExecuter/src/main/java/erdemc/JtaExecuterApplication.java
@@ -0,0 +1,12 @@
+package erdemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtaExecuterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtaExecuterApplication.class, args);
+	}
+}
