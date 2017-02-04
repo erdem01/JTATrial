087c159b4c772ebd134d0544f2e4ef1ed48f9df3
Caglayan, Erdem <erdemcaglayan_1@hotmail.com>
2017-02-04 16:13:30 +0200

    Mul-At executer implemented but failed.

--- JTAExecuter_ForMulAt/src/main/java/erdemc/JtaExecuterForMulAtApplication.java
+++ JTAExecuter_ForMulAt/src/main/java/erdemc/JtaExecuterForMulAtApplication.java
@@ -1,12 +1,51 @@
 package erdemc;
 
-import org.springframework.boot.SpringApplication;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
+
+import com.at.mul.MulAtAppConfiguration;
+import com.at.mul.domain.customer.Customer;
+import com.at.mul.domain.order.Order;
+import com.at.mul.repository.customer.CustomerRepository;
+import com.at.mul.repository.order.OrderRepository;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={HibernateJpaAutoConfiguration.class, JtaAutoConfiguration.class})
+@Import(MulAtAppConfiguration.class)
 public class JtaExecuterForMulAtApplication {
 
+	@Autowired
+	private CustomerRepository firstDAO;
+
+	@Autowired
+	private OrderRepository secondDAO;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(JtaExecuterForMulAtApplication.class, args);
+		ConfigurableApplicationContext context = new SpringApplicationBuilder().sources(JtaExecuterForMulAtApplication.class)
+				.bannerMode(Banner.Mode.OFF).run(args);
+
+		JtaExecuterForMulAtApplication app = context.getBean(JtaExecuterForMulAtApplication.class);
+		app.start();
+	}
+
+	@Transactional
+	private void start() {
+		Customer first = new Customer();
+		first.setName("customer");
+		Order second = new Order();
+		second.setCode(22);
+		firstDAO.save(first);
+		secondDAO.save(second);
+		System.out.println("Hello World!");
+		throw new RuntimeException();
 	}
 }
