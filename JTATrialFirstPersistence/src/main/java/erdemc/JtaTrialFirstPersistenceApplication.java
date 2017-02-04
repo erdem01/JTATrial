229950c5fd74dd73e3989e1105c6fd067d48ed8e
Caglayan, Erdem <erdemcaglayan_1@hotmail.com>
2017-02-04 16:15:46 +0200

    First and Second persistence projects are added.

---
+++ JTATrialFirstPersistence/src/main/java/erdemc/JtaTrialFirstPersistenceApplication.java
@@ -0,0 +1,12 @@
+package erdemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtaTrialFirstPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtaTrialFirstPersistenceApplication.class, args);
+	}
+}
---
+++ JTATrialSecondPersistence/src/main/java/erdemc/JtaTrialSecondPersistenceApplication.java
@@ -0,0 +1,12 @@
+package erdemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtaTrialSecondPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtaTrialSecondPersistenceApplication.class, args);
+	}
+}
