1d7ae178bcfd3f80657168f636bed023bb152149
Caglayan, Erdem <erdemcaglayan_1@hotmail.com>
2017-02-04 15:14:44 +0200

    Mul-at executer project added.

---
+++ JTAExecuter_ForMulAt/src/main/java/erdemc/JtaExecuterForMulAtApplication.java
@@ -0,0 +1,12 @@
+package erdemc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtaExecuterForMulAtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtaExecuterForMulAtApplication.class, args);
+	}
+}
